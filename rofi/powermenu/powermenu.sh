f992f46f821c9c14aca17c8eaa21e6f5216a5b03
Henrique Morais <hfmorais@outlook.com>
2024-06-13 21:16:29 +0100

    Added additional config files

---
+++ rofi/powermenu/powermenu.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+## Author : Aditya Shakya (adi1090x)
+## Github : @adi1090x
+#
+## Rofi   : Power Menu
+#
+## Available Styles
+#
+## style-1   style-2   style-3   style-4   style-5
+
+# Current Theme
+dir="$HOME/.config/rofi/powermenu"
+theme='style'
+
+# CMDs
+uptime="`uptime -p | sed -e 's/up //g'`"
+host=`hostname`
+
+# Options
+shutdown=''
+reboot=''
+lock=''
+suspend='⏾'
+logout=''
+yes=''
+no=''
+
+# Rofi CMD
+rofi_cmd() {
+	rofi -dmenu \
+		-p "Goodbye ${USER}" \
+		-mesg "Uptime: $uptime" \
+		-theme ${dir}/${theme}.rasi
+}
+
+# Pass variables to rofi dmenu
+run_rofi() {
+	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+    $shutdown)
+    	systemctl poweroff
+        ;;
+    $reboot)
+		systemctl reboot
+        ;;
+    $lock)
+    	bash ~/.scripts/lock-screen.sh
+        ;;
+    $suspend)
+		mpc -q pause
+		mixer set Master mute
+		systemctl suspend
+        ;;
+    $logout)
+    	bspc quit
+        ;;
+esac
