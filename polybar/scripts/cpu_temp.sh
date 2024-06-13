5440de626cd52dde5532ea30f0a8af6871b85b5a
Henrique Morais <hfmorais@outlook.com>
2024-06-13 20:36:22 +0100

    Updated cpu temp script

--- polybar/scripts/cpu_temp.sh
+++ polybar/scripts/cpu_temp.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Sensor name in lm-sensors
-sensor_name="Tccd1"
+#sensor_name="Tccd1"
+#temp_value=$(sensors | grep "$sensor_name" | awk '{print $2}' | tr -d '+')
 
-temp_value=$(sensors | grep "$sensor_name" | awk '{print $2}' | tr -d '+')
-printf "%s" "$temp_value"
+sensor_path="/sys/class/hwmon/hwmon1/temp3_input"
+sensor_value=$(cat $sensor_path)
+
+printf "%sºC" "$((sensor_value / 1000))"
